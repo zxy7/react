5ea3ba7358bd500cbee83dcef1c8b0dc49c4b6a9
昝晓玉 <xyzan@jinhetech.com>
2017-04-21 18:18:30 +0800

    save

--- app/page/Dairy.js
+++ app/page/Dairy.js
@@ -7,8 +7,9 @@ import { Link } from 'react-router'
 class Dairy extends Component {
 	constructor(props) {
 		super(props);
-		this.bold=this.bold.bind(this);
+		this.bold = this.bold.bind(this);
 		this.state = {
+			num: 0,
 		};
 	}
 
@@ -16,7 +17,7 @@ class Dairy extends Component {
 
 		return (
 			<div>
-				<header className="intro-header" style={{ backgroundImage: " url('/image/pic11.jpg')" }}>
+				<header className="intro-header" style={{ backgroundImage: " url('/image/pic12.jpg')" }}>
 					<div className="container">
 						<div className="row">
 							<div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 ">
@@ -24,7 +25,7 @@ class Dairy extends Component {
 
 									<h1>
 										<div className="ui transparent input">
-											<input type="text" style={{ textAlign: 'center', color: 'white' }} placeholder="请输入标题" />
+											<input type="text" style={{width:'100%', textAlign: 'center', color: 'white' }} placeholder="请输入标题" />
 										</div>
 									</h1>
 									<span className="subheading">
@@ -37,7 +38,7 @@ class Dairy extends Component {
 						</div>
 					</div>
 				</header>
-				<div style={{ textAlign: 'center' ,paddingTop:'10px'}}>
+				<div style={{ textAlign: 'center', paddingTop: '10px' }}>
 					<i className="photo icon"></i>
 					<i className="header icon"></i>
 					<i className="bold icon" onClick={this.bold}></i>
@@ -46,16 +47,16 @@ class Dairy extends Component {
 					<i className="save icon" ></i>
 				</div>
 
-				<div className="dairy-content "
+				<div className="dairy-content " id='content'
 					style={{
-						height: " 500px", fontSize: "14px", 
+						height: " 500px", fontSize: "14px",
 						marginLeft: " auto", marginRight: " auto"
 					}}>
 
-					<textarea type="text" placeholder="请输入正文"
+					<textarea id="msg" type="text" placeholder="请输入正文"
 						style={{
-							width: "100%", height: " 500px", outline: " 0px", border: "none", fontSize: "14px",
-							backgroundColor: "transparent", color: "rgb(0, 0, 0)", wordBreak: "break-all",paddingTop:'10px'
+							width: "100%", height: " 100px", outline: " 0px", border: "none", fontSize: "14px",
+							backgroundColor: "transparent", color: "rgb(0, 0, 0)", wordBreak: "break-all", paddingTop: '10px'
 						}}></textarea>
 
 
@@ -63,8 +64,19 @@ class Dairy extends Component {
 			</div>
 		);
 	}
-	bold(){
-		
+	bold() {
+		$('.bold.icon').toggleClass('icon-active');
+		if ($('.bold.icon').hasClass('icon-active')) {
+			// console.log(113)
+			// var str = '<input style="font-weight:bold" type="text" id = "num_' + this.state.num + '">';
+			// $('#content').append(str);
+			// $('#num_' + this.state.num).focus();
+			// this.setState({
+			// 	num:this.state.num++
+			// })
+			// console.log(1134)
+			
+		}
 	}
 
 	componentDidMount() {
